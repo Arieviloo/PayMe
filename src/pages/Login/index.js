37ea0cf5daada9f4659bb493d9d1f1ad8f099215
Arievilo <jadieoliveira00@gmail.com>
2019-10-31 21:03:40 -0300

    PayMe - Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  Container,
+  // Container,
   Button,
-  Avatar,
+  Paper,
   CssBaseline,
   TextField,
   Link,
@@ -13,14 +13,107 @@ import {
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import './style.css';
 
+// const useStyles = makeStyles(theme => ({
+//   '@global': {
+//     body: {
+//       backgroundColor: theme.palette.common.white,
+//     },
+//   },
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//   },
+//   avatar: {
+//     margin: theme.spacing(1),
+//     backgroundColor: theme.palette.secondary.main,
+//   },
+//   form: {
+//     width: '100%', // Fix IE 11 issue.
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//   },
+// }));
+
+// export default function Login() {
+//   const classes = useStyles();
+
+//   return (
+//     <Container component="main" className="bg" maxWidth="xs">
+//       <CssBaseline />
+//       <div className="form">
+//         <Typography component="h1" variant="h5" color="primary">
+//           Login
+//         </Typography>
+//         <form className={classes.form} noValidate>
+//           <TextField
+//             variant="outlined"
+//             margin="dense"
+//             required
+//             fullWidth
+//             id="email"
+//             label="Email Address"
+//             name="email"
+//             autoComplete="email"
+//             autoFocus
+//           />
+//           <TextField
+//             variant="outlined"
+//             margin="dense"
+//             required
+//             fullWidth
+//             name="password"
+//             label="Password"
+//             type="password"
+//             id="password"
+//             autoComplete="current-password"
+//           />
+
+//           <Button
+//             type="submit"
+//             fullWidth
+//             variant="contained"
+//             color="primary"
+//             className={classes.submit}
+//           >
+//             <VpnKeyIcon />
+//             Entrar
+//           </Button>
+//           <Grid container>
+//             <Grid item>
+//               <Link href="#" variant="body1">
+//                 Esqueceu a senha?
+//               </Link>
+//             </Grid>
+//             <Grid item>
+//               <Link href="#" variant="body1">
+//                 Não possui uma conta? Cadastre-se!
+//               </Link>
+//             </Grid>
+//           </Grid>
+//         </form>
+//       </div>
+//     </Container>
+//   );
+// }
+
 const useStyles = makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
+  root: {
+    height: '100px',
+    width: '800px',
+  },
+  image: {
+    backgroundImage:
+      'url(https://scproduction.s3.sa-east-1.amazonaws.com/wysiwyg_uploads/cms/images/2019/05/28/12/como-fazer-a-prestacao-de-contas-em-condominios-saiba-aqui-z3uqcw97.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   },
   paper: {
-    marginTop: theme.spacing(8),
+    margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -38,64 +131,69 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function Login() {
+export default function SignInSide() {
   const classes = useStyles();
 
   return (
-    <Container component="main" className="bg" maxWidth="xs">
-      <CssBaseline />
-      <div className="form">
-        <Typography component="h1" variant="h5" color="primary">
-          Login
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="dense"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="dense"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
+    <div className="form">
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            <VpnKeyIcon />
-            Entrar
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="#" variant="body1">
-                Esqueceu a senha?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body1">
-                Não possui uma conta? Cadastre-se!
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-    </Container>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                <VpnKeyIcon />
+                Entrar
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Esqueceu a senha?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Não tem uma conta? Cadastre-se
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
