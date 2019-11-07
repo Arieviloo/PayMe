64e11362e3ad4329f05bbe6ee282a05bdb96e824
Arievilo <jadieoliveira00@gmail.com>
2019-11-07 11:46:50 -0300

    payme

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,3 @@
-// import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firebase-firestore';
 
@@ -14,58 +13,4 @@ const firebaseConfig = {
   appId: '1:1065569000114:web:bde388099c0cb7d749c7ab',
 };
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyAHoOruLMUMlAvI4-s3DdD0SyOPhsuuBFM',
-//   authDomain: 'payme-e1e50.firebaseapp.com',
-//   databaseURL: 'https://payme-e1e50.firebaseio.com',
-//   projectId: 'payme-e1e50',
-//   storageBucket: 'payme-e1e50.appspot.com',
-//   messagingSenderId: '813994851118',
-//   appId: '1:813994851118:web:c5288cd7d1b9bf875f18bf',
-// };
-
 export default firebase.initializeApp(firebaseConfig);
-
-// class Firebase {
-//   constructor() {
-//     const firebaseConfig = {
-//       apiKey: 'AIzaSyAHoOruLMUMlAvI4-s3DdD0SyOPhsuuBFM',
-//       authDomain: 'payme-e1e50.firebaseapp.com',
-//       databaseURL: 'https://payme-e1e50.firebaseio.com',
-//       projectId: 'payme-e1e50',
-//       storageBucket: 'payme-e1e50.appspot.com',
-//       messagingSenderId: '813994851118',
-//       appId: '1:813994851118:web:c5288cd7d1b9bf875f18bf',
-//     };
-//     app.initializeApp(firebaseConfig);
-//     this.auth = app.auth();
-//     this.db = app.firestore();
-//   }
-
-//   login(email, password) {
-//     return this.auth.signInWithEmailAndPassword(email, password);
-//   }
-
-//   logout() {
-//     return this.auth.signOut();
-//   }
-
-//   async register(name, email, password) {
-//     await this.auth
-//       .createUserWithEmailAndPassword(email, password)
-//       .then(res => {
-//         alert(res.user.uid);
-//       });
-//     return this.auth.currentUser.updateProfile({
-//       displayName: name,
-//     });
-//   }
-
-//   isInitialized() {
-//     return new Promise(resolve => {
-//       this.auth.onAuthStateChanged(resolve);
-//     });
-//   }
-// }
-
-// export default new Firebase();
